b3a595c63c24ee19799d8a37c68b238779effaff
Knut-Aage-Hofseth <Knut-hof@online.no>
2024-09-18 12:13:18 +0200

    first relevant test

--- PlaywrightTests/UnitTest1.cs
+++ PlaywrightTests/UnitTest1.cs
@@ -13,21 +13,9 @@ public class ExampleTest : PageTest
     [Test]
     public async Task HasTitle()
     {
-        await Page.GotoAsync("https://playwright.dev");
 
-        // Expect a title "to contain" a substring.
-        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+        await Page.GotoAsync("http://localhost:5131/");
+        await Expect(Page.GetByRole(AriaRole.Link, new() { Name = "Persons" })).ToBeVisibleAsync();
     }
 
-    [Test]
-    public async Task GetStartedLink()
-    {
-        await Page.GotoAsync("https://playwright.dev");
-
-        // Click the get started link.
-        await Page.GetByRole(AriaRole.Link, new() { Name = "Get started" }).ClickAsync();
-
-        // Expects page to have a heading with the name of Installation.
-        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Installation" })).ToBeVisibleAsync();
-    } 
 }
